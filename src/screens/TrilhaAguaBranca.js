2385cd5d8b3f32e7efa6d031c3bf25db83fedfeb
duaksd <casado.eduarda@aluno.ifsp.edu.br>
2025-10-03 17:08:30 -0300

    imagens trilhas e atualizando texto favoritesscreen

--- src/screens/TrilhaAguaBranca.js
+++ src/screens/TrilhaAguaBranca.js
@@ -4,10 +4,10 @@ import DetailScreen from "../components/DetailScreen";
 const aguaBrancaData = {
   title: "Trilha da Água Branca",
   images: [
-    "https://www.ilhabela.com.br/imagens/trilha-agua-branca-1.jpg",
-    "https://www.ilhabela.com.br/imagens/trilha-agua-branca-2.jpg",
-    "https://www.ilhabela.com.br/imagens/trilha-agua-branca-3.jpg",
-    "https://www.ilhabela.com.br/imagens/trilha-agua-branca-4.jpg"
+    "https://www.ilhabela.tudoem.com.br/assets/img/coluna/10b07396caeabeee0b4a73463c8fe665.jpg",
+    "https://arquipelagoilhabela.com.br/wp-content/uploads/2021/01/Trilhabranca_4.jpg",
+    "https://arquipelagoilhabela.com.br/wp-content/uploads/2021/01/Trilhabranca_8.jpg",
+    "https://arquipelagoilhabela.com.br/wp-content/uploads/2021/01/Trilhabranca_12.jpg"
   ],
   description:
     "A Trilha da Água Branca em Ilhabela é uma caminhada encantadora de aproximadamente 6 km, passando por trechos de mata atlântica, pequenas cachoeiras e riachos de águas cristalinas. Ideal para quem gosta de ecoturismo, aventura e contato direto com a natureza, proporcionando um dia refrescante e revigorante.",
--- src/screens/TrilhaSetePraias.js
+++ src/screens/TrilhaSetePraias.js
@@ -4,10 +4,10 @@ import DetailScreen from "../components/DetailScreen";
 const setePraiasData = {
   title: "Trilha das Sete Praias",
   images: [
-    "https://www.guiadecaraguatatuba.com.br/wp-content/uploads/2021/08/trilha-sete-praias.jpg",
-    "https://th.bing.com/th/id/OIP.Exemplo2.jpg",
-    "https://th.bing.com/th/id/OIP.Exemplo3.jpg",
-    "https://th.bing.com/th/id/OIP.Exemplo4.jpg"
+    "https://static1.s123-cdn-static-a.com/uploads/3237687/800_60c945bf489e0.jpg",
+    "https://www.voltologo.net/wp-content/uploads/2019/02/praia-do-cedro-sete-praias.jpg",
+    "https://meudestinoelogoali.com.br/wp-content/uploads/2023/03/Trilha-das-Sete-Praias-Ubatuba-@meudestinoelogoali-1024x725.jpg",
+    "https://images.sympla.com.br/5de552f58d9d2.jpg"
   ],
   description:
     "A Trilha das Sete Praias em Ubatuba é uma caminhada deslumbrante de aproximadamente 10 km, conectando praias selvagens e paradisíacas. Iniciando na Praia da Fortaleza ou na Praia da Lagoinha, o percurso inclui praias como Cedro do Sul, Prainha da Deserta, Deserta, Grande do Bonete, Bonete, Peres e Oeste. Ideal para quem busca ecoturismo, aventura e contato direto com a natureza.",
