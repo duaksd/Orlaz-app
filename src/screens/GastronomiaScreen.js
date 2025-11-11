f23055b8ea30e99f5bfd7ede5edc29cc4bc1fb15
Julioklyn <julio.carlos@aluno.ifsp.edu.br>
2025-11-11 17:08:20 -0300

    ajuste img sabores do mar

--- src/screens/GastronomiaScreen.js
+++ src/screens/GastronomiaScreen.js
@@ -23,7 +23,7 @@ export default function GastronomiaScreen({ navigation }) {
       name: "Sabores do Mar",
       location: "Ubatuba",
       description: "Variedade de frutos do mar preparados com tradição.",
-      image: { uri: "https://scontent.fpoa10-1.fna.fbcdn.net/v/t39.30808-6/482053788_3490971341047763_5679505573610082876_n.jpg?stp=dst-jpg_s960x960_tt6&_nc_cat=108&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=XoUksaxVtVcQ7kNvwEAnG_F&_nc_oc=Admxpg1ztG8jAoOeD9xdWNajfkqlq3eFOmQ6mQ6oXxscyPyolRBsU__x9R5CRewpxaA&_nc_zt=23&_nc_ht=scontent.fpoa10-1.fna&_nc_gid=ENXmD7K3lbmGYkYRxD41zg&oh=00_AffT4DRA38qAoVnAhRkhVkevDddFuCPi5N36JK6fCN2Yzg&oe=68EB4EE5" },
+      image: { uri: "https://d2kihw5e8drjh5.cloudfront.net/eyJidWNrZXQiOiJ1dGEtaW1hZ2VzIiwia2V5IjoicGxhY2VfaW1nL0s2UEhxR3dHUjNhcVhZcjlPTzNrZmciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjY0MCwiaGVpZ2h0Ijo2NDAsImZpdCI6Imluc2lkZSJ9LCJyb3RhdGUiOm51bGwsInRvRm9ybWF0IjogIndlYnAifX0=" },
       route: "SaboresDoMar"
     },
   ];
